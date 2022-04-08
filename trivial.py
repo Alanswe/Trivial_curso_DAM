872d0ffe6c596d41ba217f5dbb9e2d6864669be8
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-04-08 12:46:51 +0200

    main

---
+++ trivial.py
@@ -0,0 +1,21 @@
+from partida import Partida
+from jugador import Jugador
+from pregunta import Pregunta
+from respuesta import Respuesta
+from modo import Modo
+
+# Creación de una pregunta y sus respuestas
+
+p1 = Pregunta(1,'¿Es verano?')
+r1 = Respuesta(1,'Sí',False)
+r2 = Respuesta(2,'No',True)
+
+p1.respuestas = [r1,r2]
+col_preguntas = [p1]
+
+j = Jugador('Teo')
+m = Modo()
+
+mi_partida = Partida(j,m,col_preguntas)
+
+mi_partida.iniciar()
