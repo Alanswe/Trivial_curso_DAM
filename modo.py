01b15a2f3ab2331b59f7c40f5a18fd47c4d1be5c
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-04-08 11:12:01 +0200

    modo

---
+++ modo.py
@@ -0,0 +1,26 @@
+from re import I
+
+
+class Modo():
+    def __init__(self,num_preguntas=10,num_jugadores=1,tmax_juego=600,tmax_pregunta=600) -> None:
+        self.__num_preguntas = num_preguntas
+        self.__num_jugadores = num_jugadores
+        self.__tmax_juego = tmax_juego
+        self.__tmax_pregunta = tmax_pregunta
+
+
+    @property
+    def num_preguntas(self):
+        return self.__num_preguntas
+
+    @property
+    def num_jugadores(self):
+        return self.__num_jugadores
+        
+    @property
+    def tmax_juego(self):
+        return self.__tmax_juego
+
+    @property
+    def tmax_pregunta(self):
+        return self.__tmax_pregunta
