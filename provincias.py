f24bbb88e5c857fa9219d3d26fa00be95eaf682d
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-04-22 11:29:28 +0200

    22/04

--- provincias.py
+++ provincias.py
@@ -9,16 +9,35 @@ class Provincia():
         self.__descripcion = descripcion
         self.__id = id
 
-    def insertar(self):
-        manejador = Sqlite(BD)
-        campos = 'codigo,descripcion'
-        valores = f'{self.__codigo},{self.__descripcion}'
-        nuevo_id = manejador.insertar(Provincia.tabla,campos,valores)
-        return nuevo_id
+    # def insertar(self):
+    #     # obsoleto
+    #     manejador = Sqlite(BD)
+    #     campos = 'codigo,descripcion'
+    #     valores = f'{self.__codigo},{self.__descripcion}'
+    #     nuevo_id = manejador.insertar(Provincia.tabla,campos,valores)
+    #     return nuevo_id
 
-    def actualizar(self):
+    def guardar(self):
         manejador = Sqlite(BD)
         campos = 'codigo,descripcion'
         valores = f'{self.__codigo},{self.__descripcion}'
-        nuevo_id = manejador.actualizar(Provincia.tabla,campos,valores,self.__id)
-        return nuevo_id
+        if self.__id:
+            nuevo_id = manejador.actualizar(Provincia.tabla,campos,valores,self.__id)
+            return nuevo_id
+        else:
+            nuevo_id = manejador.insertar(Provincia.tabla,campos,valores)
+            return nuevo_id
+
+    def leer_por_id(self,id_privincia):
+        manejador = Sqlite(BD)
+        lista = manejador.seleccionar(f'select * from T_provincias where id = {id_privincia}') # NO HACER ASI POR SEGURIDAD, SOLO ES UN EJEMPLO
+        if lista:
+            return Provincia(lista[0][1],lista[0][2],lista[0][0])
+        else:
+            return None
+
+    def borrar(self,id_provincia):
+        manejador = Sqlite(BD)
+        resp = manejador.borrar(Provincia.tabla,'id',id_provincia)
+        return resp
+        
--- pruebas/test_provincias.py
+++ pruebas/test_provincias.py
@@ -10,12 +10,32 @@ class Test_provicias(unittest.TestCase):
         p = Provincia('','')
         self.assertIsNotNone(p)
         
-    def test_insert_provincia(self):
-        p = Provincia(211,'Madrid')
-        resp = p.insertar()
+    # def test_insert_provincia(self):
+    #     p = Provincia(211,'Madrid')
+    #     resp = p.insertar()
+    #     self.assertIsNotNone(resp)
+
+    def test_guardar_provincia_nueva(self):
+        p = Provincia(646,'Francia')
+        resp = p.guardar()
         self.assertIsNotNone(resp)
 
-    def test_actualizar_provincia(self):
-        p = Provincia(662,'Marruecos',5)
-        resp = p.actualizar()
+    def test_guardar_provincia_existente(self):
+        p = Provincia(888,'Marruecos',5)
+        resp = p.guardar()
+        self.assertEqual(resp,1)
+
+    def test_leer_por_id_no_existente(self):
+        p = Provincia('','')
+        resp = p.leer_por_id(199)
+        self.assertEqual(resp,'turutu')
+
+    def test_leer_por_id_existente(self):
+        p = Provincia('','')
+        resp = p.leer_por_id(1) #Huelva
+        self.assertEqual(resp,'turutu')
+
+    def test_borrar(self):
+        p = Provincia('','')
+        resp = p.borrar(7)
         self.assertIsNotNone(resp)
